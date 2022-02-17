c2af17bb75352e76f184c2b713cd9856577a28c2
Valeriy Dutt <valerydutt@gmail.com>
2022-02-17 15:58:26 +0000

    finalized express server setup

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -4,7 +4,11 @@ const dotenv = require('dotenv');
 // Load env vars
 dotenv.config({ path: './config/config.env'})
 
-const app = express()
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from express');
+})
 
 const PORT = process.env.PORT || 5000;
 
