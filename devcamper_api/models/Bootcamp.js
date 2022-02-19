ee03c31b447cef7a2d80736061c628299b650c7a
Valeriy Dutt <valerydutt@gmail.com>
2022-02-19 22:41:44 +0000

    added slugify

--- devcamper_api/models/Bootcamp.js
+++ devcamper_api/models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -95,6 +96,12 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+});
+
+// Create bootcamp slug from name
+BootcampSchema.pre('save', function(next){
+    this.slug = slugify(this.name, {lower: true});
+    next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
