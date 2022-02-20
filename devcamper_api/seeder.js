47e04e7123fe3dbe3dde6015b673661eebdb926b
Valeriy Dutt <valerydutt@gmail.com>
2022-02-20 21:26:10 +0000

    added a db seeder

---
+++ devcamper_api/seeder.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({path: './config/config.env'});
+
+// Load models
+const Bootcamp = require('./models/Bootcamp');
+
+mongoose.connect(process.env.MONGO_URI);
+
+// Read JSON files
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+
+// Import into db
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+
+        console.log('Data imported...'.green.inverse)
+        process.exit();
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// Delete data
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+
+        console.log('Data destroyed...'.red.inverse)
+        process.exit();
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+if(process.argv[2] === '-i'){
+    importData();
+} else if(process.argv[2] ==='-d'){
+    deleteData();
+}
