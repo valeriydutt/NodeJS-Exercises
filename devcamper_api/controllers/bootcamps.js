c0188a178235dec3a22f0ce790c8028409ca9ba0
Valeriy Dutt <valerydutt@gmail.com>
2022-02-21 22:19:57 +0000

    pagination limit fix

--- devcamper_api/controllers/bootcamps.js
+++ devcamper_api/controllers/bootcamps.js
@@ -44,7 +44,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
     // Pagination
     const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 100;
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
     const total = await Bootcamp.countDocuments();
