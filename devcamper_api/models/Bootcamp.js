17738bb5c149e07ae7a2d5937fb197db1291f455
Valeriy Dutt <valerydutt@gmail.com>
2022-02-21 23:03:06 +0000

    added virtual course collection to bootcamp & cascade delete

--- devcamper_api/models/Bootcamp.js
+++ devcamper_api/models/Bootcamp.js
@@ -97,6 +97,9 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 });
 
 // Create bootcamp slug from name
@@ -121,8 +124,22 @@ BootcampSchema.pre('save', async function(next) {
 
     // Do not save address in db
     this.address = undefined;
-
     next();
-})
+});
+
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+    console.log(`Courses being removed from bootcamp ${this._id}`);
+    await this.model('Course').deleteMany({ bootcamp: this._id });
+    next();
+});
+
+// Reverse populate with virtuas
+BootcampSchema.virtual('courses', {
+    ref:'Course',
+    localField: '_id',
+    foreignField: 'bootcamp',
+    justOne: false
+});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
