107c6b5fd4d75d330c94ebd9d0b568c0f5273204
Valeriy Dutt <valerydutt@gmail.com>
2022-02-21 22:31:29 +0000

    added course model

--- devcamper_api/seeder.js
+++ devcamper_api/seeder.js
@@ -8,16 +8,18 @@ dotenv.config({path: './config/config.env'});
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
-
+const Course = require('./models/Course');
 mongoose.connect(process.env.MONGO_URI);
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
 
 // Import into db
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
 
         console.log('Data imported...'.green.inverse)
         process.exit();
@@ -31,6 +33,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
 
         console.log('Data destroyed...'.red.inverse)
         process.exit();
