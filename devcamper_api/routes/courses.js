f23f5cda0f33ad1b88e7201e67acb8638bf9c9a0
Valeriy Dutt <valerydutt@gmail.com>
2022-02-22 22:45:04 +0000

    added update and delete course functionality

--- devcamper_api/routes/courses.js
+++ devcamper_api/routes/courses.js
@@ -2,7 +2,9 @@ const express = require('express');
 const { 
     getCourses,
     getCourse,
-    addCourse
+    addCourse,
+    updateCourse,
+    deleteCourse
 } = require('../controllers/courses')
 
 const router = express.Router({ mergeParams: true });
@@ -11,6 +13,9 @@ router.route('/')
     .get(getCourses)
     .post(addCourse);
 
-router.route('/:id').post(addCourse);
+router.route('/:id')
+    .get(getCourse)
+    .put(updateCourse)
+    .delete(deleteCourse);
 
 module.exports = router;
