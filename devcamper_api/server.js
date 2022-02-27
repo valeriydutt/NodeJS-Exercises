ac444f5be662c706d8519a4f3f495a9ee92146ca
valeriydutt <valerydutt@gmail.com>
2022-02-27 21:47:13 +0000

    added authentication controller & route

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -16,6 +16,8 @@ connectDB();
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
+
 
 const app = express();
 
@@ -36,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler)
 
