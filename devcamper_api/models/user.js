7f23791734faf6742ed9b72be98b1ed053ce3671
valeriydutt <valerydutt@gmail.com>
2022-02-27 22:02:27 +0000

    added password encryption

--- devcamper_api/models/user.js
+++ devcamper_api/models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const slugify = require('slugify');
-const geocoder = require('../util/geocoder');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -33,6 +32,12 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+});
+
+// Encrypt password using bcrt
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model('User', UserSchema);
