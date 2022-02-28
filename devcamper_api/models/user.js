070c8b9a40b0f16f76e05beff95695192e6edea7
valeriydutt <valerydutt@gmail.com>
2022-02-28 14:42:23 +0000

    added user login

--- devcamper_api/models/user.js
+++ devcamper_api/models/user.js
@@ -48,4 +48,11 @@ UserSchema.methods.getSignedJwtToken = function() {
     });
 }
 
+// Match user entered password to hashed password in the db
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+
+    return await bcrypt.compare(enteredPassword, this.password);
+
+}
+
 module.exports = mongoose.model('User', UserSchema);
