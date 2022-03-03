eda1ead68509e9fb14827fc68dbbc19d20c5471d
valeriydutt <valerydutt@gmail.com>
2022-03-03 12:41:45 +0000

    updated seeder to include user data

--- devcamper_api/seeder.js
+++ devcamper_api/seeder.js
@@ -9,17 +9,20 @@ dotenv.config({path: './config/config.env'});
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 mongoose.connect(process.env.MONGO_URI);
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
 
 // Import into db
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
 
         console.log('Data imported...'.green.inverse)
         process.exit();
@@ -34,6 +37,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
 
         console.log('Data destroyed...'.red.inverse)
         process.exit();
