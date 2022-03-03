4d75787b72facc3007aa5a41d8ea21294da1cb3f
valeriydutt <valerydutt@gmail.com>
2022-03-03 13:17:29 +0000

    added course ownership
    only bootcamp owner/admin can add/update courses belonging to a bootcamp

--- devcamper_api/controllers/courses.js
+++ devcamper_api/controllers/courses.js
@@ -54,6 +54,7 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 exports.addCourse = asyncHandler(async (req, res, next) => {
     
     req.body.bootcamp = req.params.bootcampId;
+    req.body.user = req.user.id;
 
     const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
@@ -61,6 +62,11 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`),
         404
         );
+    }
+
+    // Make sure user is bootcamp owner or an admin
+    if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next(new ErrorResponse(`User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`, 401));
     }
 
     const course = await Course.create(req.body);
@@ -83,6 +89,11 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse(`No course with the id of ${req.params.id}`),
         404
         );
+    }
+
+    // Make sure user is bootcamp owner or an admin
+    if(course.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next(new ErrorResponse(`User ${req.user.id} is not authorized to update course ${course._id}`, 401));
     }
 
     course = await Course.findByIdAndUpdate(req.params.id, req.body, {
@@ -108,6 +119,11 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse(`No course with the id of ${req.params.id}`),
         404
         );
+    }
+
+    // Make sure user is bootcamp owner or an admin
+    if(course.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next(new ErrorResponse(`User ${req.user.id} is not authorized to delete course ${course._id}`, 401));
     }
 
     await course.remove();
