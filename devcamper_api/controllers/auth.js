043e9a1187801295c258262dca8ec49ae3c0c23d
valeriydutt <valerydutt@gmail.com>
2022-03-09 23:50:02 +0000

    fixed URL

--- devcamper_api/controllers/auth.js
+++ devcamper_api/controllers/auth.js
@@ -108,6 +108,7 @@ exports.forgotPassword = asyncHandler(async(req, res, next) => {
 
     // Create reset url
     const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/resetpassword/${resetToken}`;
 
     const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
 
